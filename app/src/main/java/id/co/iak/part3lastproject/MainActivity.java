2ef7829e62d838f9efd8acf81c58b7ada4f9a355
Mochamad Iqbal Dwi Cahyo <razeluxe20@gmail.com>
2017-04-22 19:00:46 +0700

    [Add] Button functional, display an output @id/totalprice

--- app/src/main/java/id/co/iak/part3lastproject/MainActivity.java
+++ app/src/main/java/id/co/iak/part3lastproject/MainActivity.java
@@ -1,13 +1,33 @@
 package id.co.iak.part3lastproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Views
+    private TextView txtTotalPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        txtTotalPrice = (TextView) findViewById(R.id.txt_totalprice);
+    }
+
+
+    public void cash(View v) {
+        txtTotalPrice.setText("Rp. Tunai");
+    }
+
+    public void voucher(View v) {
+        txtTotalPrice.setText("Rp. Voucher");
     }
 }
